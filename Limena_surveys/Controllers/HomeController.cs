c32f9ee0f578dc6a2a0e9c21cf7be15fbcf176d7
Francisco Velásquez <f.velasquez@limenainc.net>
2018-07-24 16:28:13 -0600

    Limena Surveys App

--- Limena_surveys/Controllers/HomeController.cs
+++ Limena_surveys/Controllers/HomeController.cs
@@ -13,16 +13,10 @@ namespace Limena_surveys.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
-
-        public ActionResult Contact()
+        public ActionResult SurveyCM()
         {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
         }
